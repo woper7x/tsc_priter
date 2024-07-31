cbfa88515f64221fcab5ea7710cbcff6ebdc981a
nithisxk <nithisak5508@gmail.com>
2024-07-31 12:58:54 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_bluetooth_printer/flutter_simple_bluetooth_printer.dart';
@@ -107,6 +108,7 @@ class _MyAppState extends State<MyApp> {
         "^XA\r\n^MMT\r\n^PW384\r\n^LL0253\r\n^LS0\r\n^BY2,3,81^FT375,92^BCI,,N,N\r\n^FDILP-107661^FS\r\n^FT375,191^A0I,45,45^FH\\^FDILP-107661^FS\r\n^FT374,27^A0I,23,24^FH\\^FD^FS\r\n^FT372,59^A0I,23,24^FH\\^FD^FS\r\n^PQ1,0,1,Y^PQ1^XZ\r\n";
 
     try {
+      log("sending" as num);
       await _connectDevice();
       if (!_isConnected) return;
       final isSuccess = await bluetoothManager.writeText(codes);
